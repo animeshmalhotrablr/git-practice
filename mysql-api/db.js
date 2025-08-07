1d2c5ec6058a263c74abaff3603c49f8408e76d5
Animesh <animesh.tradepeeps@gmail.com>
2025-08-07 21:27:13 +0530

    added env file and relative changes

--- mysql-api/db.js
+++ mysql-api/db.js
@@ -1,11 +1,14 @@
+// Load environment variables from .env file
+require('dotenv').config()
+
 const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
-  host: 'mysql-713b71f-searchkota2021-4fa5.l.aivencloud.com',
-  user: 'avnadmin',
-  password: 'AVNS_zjOCKPVT6FEgwOre0SI',
-  database: 'defaultdb',
-  port: 10073, // <-- Added port configuration
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
